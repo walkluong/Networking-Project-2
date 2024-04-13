992551e0876da914c2646ac757d4a94b45f37699
Ricky Roberts <Richard.robertsf@gmail.com>
2024-04-13 19:36:35 -0400

    unique username check, fixed issue where sockets didn't close when leaving, more robustness changes

--- Client.py
+++ Client.py
@@ -1,6 +1,8 @@
 import socket
 import threading
+import os
 
+prompt = "Enter your message: "
 
 def client_program(): # Create a client socket and connect to the server
     host = 'localhost'
@@ -8,26 +10,40 @@ def client_program(): # Create a client socket and connect to the server
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host, port))
     
-    username = input("Enter your username: ") # Get username input from the client and send it to the server
+    username = input("Enter your username: ") # Get username input from the client and send it to the server      
     client_socket.send(username.encode())
+    
+    # if username is a duplicate then reprompt
+    while True:
+        message = client_socket.recv(1024).decode()
+        if message == "taken":
+            username = input("Username taken, enter a new username: ")
+            client_socket.send(username.encode())
+        else: 
+            break
+    
     threading.Thread(target=receive_messages, args=(client_socket,)).start()
 
     while True:
-        message = input("Enter your message: ") # Allow the client to send messages
-        if message == "quit": # If the client types 'quit', close the connection
-            client_socket.send(b'')
-            client_socket.close()
-            break
+        message = input(prompt) # Allow the client to send messages
         client_socket.send(message.encode())
+        if message == "quit": # If the client types 'quit', close the connection
+            client_socket.send(message.encode())
+            client_socket.close()
+            os._exit(0) # instead of break, kills client
 
 def receive_messages(client_socket): # Continuously receive and print messages from the server
+    
+    print(client_socket.recv(1024).decode()) # handles different formatting of initial message
     while True: 
         try:
             message = client_socket.recv(1024).decode()
             if message:
-                print(message)
+                print('\r{}\n{}'.format(message, prompt), end = '')
         except:
-            print("You have been disconnected.")
-            break
+            print("\r\nYou have been disconnected.")
+            os._exit(0) # instead of break, kills client
 
 client_program()
+
+# Personal\GitHub\Networking-Project-2
--- Server.py
+++ Server.py
@@ -7,52 +7,71 @@ messages = [] # List to store messages
 
 def client_thread(conn, addr):
     username = conn.recv(1024).decode() # Receive the username from the client
+
+    # Check for duplicate usernames
+    while [item for item in clients if item[1] == username]:
+        conn.send("taken".encode())
+        username = conn.recv(1024).decode()
+    conn.send("Valid username!".encode())
+    
     welcome_message = f"{username} has joined the chat."
     broadcast(welcome_message, "") # Notify all clients that a new user has joined
-    clients.append((conn, username)) # Add new client/user to the group
+    
+    # Create a tuple for each connection and username to more easily handle clients in loops
+    client_ID = (conn, username)
+    
+    clients.append(client_ID) # Add new client/user to the group
     send_recent_messages(conn) # Send the last two messages to the new client
 
     while True:
         try:
             message = conn.recv(1024).decode() #make sure the message is normal, otherwise will skip the loop, and move to the next one.
-            if message: # If message is normal, format the message as:  “Message ID, Sender, Post Date, Subject"
+            if message == 'quit':
+                remove(client_ID)
+                broadcast(f"{username} has left the chat.", "")
+                break
+            elif message: # If message is normal, format the message as:  “Message ID, Sender, Post Date, Subject"
                 formatted_message = f"{len(messages)+1}, {username}, {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}, {message}"
                 messages.append(formatted_message) # Store message into the list
                 broadcast(formatted_message, username) #send message to all clients in the group
             else:
-                remove(conn)
+                remove(client_ID)
                 break
         except: # If something is wrong with the message, the code will just move to the next message
             continue
 
 def broadcast(message, sender): # Send a message to all connected clients except the sender
-    for client, user in clients:
-        if user != sender:
-            client.sendall(message.encode())
+    for c in clients:
+        if c[1] != sender:
+            c[0].sendall(message.encode())
 
 def send_recent_messages(conn): # Send the last two messages from the history to a new client
-    for message in messages[-2:]:
-        conn.sendall(message.encode() + b'\n')
+    if len(messages) == 0:
+        conn.sendall("No recent messages".encode() + b'\n')
+    else:
+        for message in messages[-2:]:
+            conn.sendall(message.encode() + b'\n')
 
-def remove(connection): # Remove a client that has disconnected and notify others
-    for i, (conn, username) in enumerate(clients):
-        if conn == connection:
-            conn.close()
-            clients.pop(i)
-            broadcast(f"{username} has left the chat.", "")
+def remove(client_ID): # Remove a client that has disconnected and notify others
+    for c in clients:
+        if c == client_ID:
+            c[0].close()
+            clients.remove(c)
             break
 
 def start_server(): # Create a socket and bind it to a host and port
     host = 'localhost'
     port = 12345
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((host, port))
     server_socket.listen()
-    print("Server is listening...")
+    print(f"Server is listening on [{server_socket.getsockname()}, Port: {port}]")
 
     while True:
         conn, addr = server_socket.accept() # Accept new connections
         print(f"Connected to {addr}")
         threading.Thread(target=client_thread, args=(conn, addr)).start() # Start a new thread for each connected client
+
 
 start_server()
